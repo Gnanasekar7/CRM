6020014d32870cb18b07e2e1ed81d7ef6b097880
Gnanasekar K <gnanasekar.k@optisolbusiness.com>
2023-03-17 10:34:10 +0530

    third commit

--- src/Reg.js
+++ src/Reg.js
@@ -20,9 +20,20 @@ function Reg () {
         event.preventDefault() 
        let hasError = false
        let errors=initialStateErrors 
+       let regularExpression= /^[a-zA-Z0-9!@#$%^&*]{6,16}$/
        
-       if( inputs.fname === '' ) {
+       if( inputs.fname === ''
+    //     && inputs.lname === '' && inputs.email === '' && inputs.pass === '' 
+    //    && inputs.pass === '' && inputs.cpass === '' && inputs.pno === '')
+        ) {
             errors.fname.required = true
+            // errors.lname.required = true
+            // errors.email.required = true
+            // errors.pass.required = true
+            // errors.cpass.required =true
+            // errors.pno.required = true
+
+
             hasError=true
        }
        if(inputs.lname === ''){
@@ -33,10 +44,19 @@ function Reg () {
         errors.email.required = true
         hasError=true
        }
-       if(inputs.pass === '' && inputs.pass.length<8){
+       if(inputs.pass === '' ){
         errors.pass.required = true
         hasError=true
        }
+       
+       if( inputs.pass !==''  ){
+       if(!regularExpression.test(inputs.pass) ){
+       
+        alert("password should contain atleast one number and one special character")
+        hasError=true
+       }
+    }
+
        if(inputs.cpass === ''){
         errors.cpass.required =true
         hasError=true
@@ -74,45 +94,55 @@ function Reg () {
                 <div className='title'>SignUp</div>
                 <div className='inputs'>
                     <form onSubmit={handleSubmit}>
+                        <label>First Name    </label>
                         <input type='text' placeholder='First Name' name='fname' onChange={handleInput}></input>
+                        <br/>
                         {errors.fname.required?
                         (<span className="text-danger" >
                                      First Name is required.
                                 </span>):null
                                 }                       
-                        <br></br>
+                        <br/>
+                        <label>Last Name    </label>
                         <input type='text' placeholder='Last Name' name='lname' onChange={handleInput} ></input>
+                        <br/>
                         {errors.lname.required?
                         (<span className="text-danger" >
                                      last Name is required.
                                 </span>):null
                                 }
-                        <br></br>
+                        <br/>
+                        <label>Email    </label>
                         <input type='email' placeholder="Email" name='email' onChange={handleInput}></input>
+                        <br/>
                         {errors.email.required?
                         (<span className="text-danger" >
                                      Email is required.
                                 </span>):null
                                 }
-                        <br></br>
+                        <br/>
+                        <label>PhoneNumber    </label>
                         <input type='text' placeholder='88888-88888' name='pno' onChange={handleInput}></input>
+                        <br/>
                         {errors.pno.required?
                         (<span className="text-danger" >
                                      phNo is required.
                                 </span>):null
                                 }
-                        <br></br>
+                        <br/>
+                        <label>Password    </label>
                         <input type='password' placeholder="password" name='pass' onChange={handleInput}></input>
+                        <br/>
                         {errors.pass.required?
                         (<span className="text-danger" >
                                      Password is required.
                                 </span>):null
                                 }
-                        <br></br>
+                        <br/>
+                        <label>confirmPassword    </label>
                         <input type='password' placeholder="Confirm Password" name='cpass' onChange={handleInput}></input>
-                        <br></br>
-                        
-
+                        <br/>
+                        <br/>
                         <input type='submit' value='Register'/>
                     </form>
                 </div>
